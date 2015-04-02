d477d7e2f3c664870ad4329cc66d954c3b35e61f
RobertSnodgrass <robert.snodgrass@gmail.com>
2015-04-02 05:28:10 -0500

    adding finished koans

--- koans/AboutHigherOrderFunctions.js
+++ koans/AboutHigherOrderFunctions.js
@@ -11,17 +11,17 @@ describe("About Higher Order Functions", function() {
     var numbers = [1,2,3];
     var odd     = _(numbers).filter(function(x) { return x % 2 !== 0 });
     
-    expect(odd).toEqual(FILL_ME_IN);
-    expect(odd.length).toBe(FILL_ME_IN);
-    expect(numbers.length).toBe(FILL_ME_IN);
+    expect(odd).toEqual([1, 3]);
+    expect(odd.length).toBe(2);
+    expect(numbers.length).toBe(3);
   });
     
   it("should use 'map' to transform each element", function() {
     var numbers      = [1, 2, 3];
     var numbersPlus1 = _(numbers).map(function(x) { return x + 1 });
     
-    expect(numbersPlus1).toEqual(FILL_ME_IN);
-    expect(numbers).toEqual(FILL_ME_IN);
+    expect(numbersPlus1).toEqual([2, 3, 4]);
+    expect(numbers).toEqual([1, 2, 3]);
   });
     
   it("should use 'reduce' to update the same result on each iteration", function () {
@@ -35,8 +35,8 @@ describe("About Higher Order Functions", function() {
       /* initial */ 0
     );
     
-    expect(reduction).toBe(FILL_ME_IN);
-    expect(numbers).toEqual(FILL_ME_IN);
+    expect(reduction).toBe(6);
+    expect(numbers).toEqual([1, 2, 3]);
   });
     
   it("should use 'forEach' for simple iteration", function() {
@@ -48,8 +48,8 @@ describe("About Higher Order Functions", function() {
 
     _(numbers).forEach(isEven);
     
-    expect(msg).toEqual(FILL_ME_IN);
-    expect(numbers).toEqual(FILL_ME_IN);
+    expect(msg).toEqual("falsetruefalse");
+    expect(numbers).toEqual([1, 2, 3]);
   });
     
   it("should use 'all' to test whether all items pass condition", function() {
@@ -58,8 +58,8 @@ describe("About Higher Order Functions", function() {
 
     var isEven = function(x) { return x % 2 === 0 };
 
-    expect(_(onlyEven).all(isEven)).toBe(FILL_ME_IN);
-    expect(_(mixedBag).all(isEven)).toBe(FILL_ME_IN);
+    expect(_(onlyEven).all(isEven)).toBe(true);
+    expect(_(mixedBag).all(isEven)).toBe(false);
   });
     
   it("should use 'any' to test if any items passes condition" , function() {
@@ -68,28 +68,28 @@ describe("About Higher Order Functions", function() {
 
     var isEven = function(x) { return x % 2 === 0 };
 
-    expect(_(onlyEven).any(isEven)).toBe(FILL_ME_IN);
-    expect(_(mixedBag).any(isEven)).toBe(FILL_ME_IN);
+    expect(_(onlyEven).any(isEven)).toBe(true);
+    expect(_(mixedBag).any(isEven)).toBe(true);
   });
 
   it("should use range to generate an array", function() {
-    expect(_.range(3)).toEqual(FILL_ME_IN);
-    expect(_.range(1, 4)).toEqual(FILL_ME_IN);
-    expect(_.range(0, -4, -1)).toEqual(FILL_ME_IN);
+    expect(_.range(3)).toEqual([0, 1, 2]);//Only one value = starting point is 0; Goes from 0 - 3 but does not include 3 (uninclusive)
+    expect(_.range(1, 4)).toEqual([1, 2, 3]);//Two values = starting point is 1, 4 is upper limit, uninclusive, so 3
+    expect(_.range(0, -4, -1)).toEqual([0, -1, -2, -3], [0]);//Start with 0, -4 is lower end limit, uninclusive, so -3; second array is 0 to -1 uninclusive, so only one value
   });
 
   it("should use flatten to make nested arrays easy to work with", function() {
-    expect(_([ [1, 2], [3, 4] ]).flatten()).toEqual(FILL_ME_IN);
+    expect(_([ [1, 2], [3, 4] ]).flatten()).toEqual([1, 2, 3, 4]);
   });
 
   it("should use chain() ... .value() to use multiple higher order functions", function() {
     var result = _([ [0, 1], 2 ]).chain()
-                     .flatten()
-                     .map(function(x) { return x+1 } )
-                     .reduce(function(sum, x) { return sum + x })
-                     .value();
+                     .flatten()//combine both arrays into [0, 1, 2]
+                     .map(function(x) { return x+1 } )//maps +1 to all values; [1, 2, 3]
+                     .reduce(function(sum, x) { return sum + x })//Incudes 0 , 1 -> reduce to 1; 1, 2 -> reduce to 3; 3, 3 -> reduce to 6
+                     .value();//Gets value of array -> 6
 
-    expect(result).toEqual(FILL_ME_IN);
+    expect(result).toEqual(6);
   });
 
 });
